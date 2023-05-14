12f5a54331d60eed9eb0a319a9baa2a3f284ab10
Vishal Kumar <kushwaha.evil600@gmail.com>
2023-05-14 19:35:39 +0530

    debugging set to false

--- Quiz/Quiz/settings.py
+++ Quiz/Quiz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-il8f7v&_k0s+slbp0_=0sa#@+r663t%8-^-^@30_xss=)a1e+i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['quizes-django.vercel.app','127.0.0.1']
 
